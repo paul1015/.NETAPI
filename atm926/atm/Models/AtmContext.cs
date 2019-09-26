82cc4859c773c1a98a4920a1972280931e414efe
paul <paul@LIUde-Mac-mini.local>
2019-09-26 13:03:36 +0800

    atm 926

---
+++ atm926/atm/Models/AtmContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+//Server's memory unused
+
+namespace atm.Models
+{
+    public class AtmContext : DbContext
+    {
+        public AtmContext(DbContextOptions<AtmContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AtmItem> AtmItems { get; set; }
+
+    }
+}
